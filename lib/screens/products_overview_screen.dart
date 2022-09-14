7fa60f80eec06d620414fc3399819d68ca785eae
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-14 12:16:14 +0500

    Adding Filtering Logic

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
-import '../providers/product.dart';
 import '../widgets/products_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FiltersOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
 
   @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavorites = false;
+  @override
   Widget build(BuildContext context) {
+    //final productContainer = Provider.of<Products>(context, listen: false,);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FiltersOptions selectedValue) {
+              setState(() {
+                if(selectedValue == FiltersOptions.Favorites)
+              {
+                _showOnlyFavorites=true;
+                //productContainer.showFavoritesOnly();
+              }else{
+                _showOnlyFavorites=false;
+               // productContainer.showAll();
+              }
+              });
+            },
+            child: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(child: Text('Only Favorites'), value: FiltersOptions.Favorites,),
+              PopupMenuItem(child: Text('Show All'), value: FiltersOptions.All,),
+          ]),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
