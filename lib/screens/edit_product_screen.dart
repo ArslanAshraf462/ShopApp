bb91d306cb5c6154617d5c2f2cbfce39d652ef3b
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-16 15:45:58 +0500

    Handling errors gracefully

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -101,6 +101,19 @@ class _EditProductScreenState extends State<EditProductScreen> {
       Navigator.of(context).pop();
     }else{
        Provider.of<Products>(context, listen: false).addProduct(_editedProduct)
+       .catchError((error){
+       return showDialog(context: context, builder: (ctx)=> AlertDialog(
+          title: Text('An error occurred!'),
+          content: Text('Something went wrong.'),
+          actions: [
+            TextButton(onPressed: (){
+              Navigator.of(ctx).pop();
+            }, 
+            child: Text('Okay'),
+            ),
+          ],
+        ));
+       })
        .then((_) { 
         setState(() {
       _isLoading=false;
