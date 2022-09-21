bf73fe9474de7d7ce80d8db62b67e5406a170e9a
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-09-21 18:13:16 +0500

    Allowing user to login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,13 +8,23 @@ class Auth with ChangeNotifier{
   late DateTime _expiryDate;
   late String _userId;
 
-  Future<void> signup(String email, String password) async{
-    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBCthS-cif6Fix_yB5vWoVAjtLB35PEQN0';
+  Future<void> _authenticate(String email, String password, String urlSegment) async{
+     final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBCthS-cif6Fix_yB5vWoVAjtLB35PEQN0';
     final response = await http.post(Uri.parse(url),body: json.encode({
       'email' : email,
       'password' : password,
       'returnSecureToken' : true,
-    },),);
-    print(json.decode(response.body));
+    },
+    ),
+    );
+     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async{
+   return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email,String password) async{
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
